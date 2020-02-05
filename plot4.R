cb51baf5adc3bebbb58f9f4fbfd496493675036b
Facundo Otrino <fotrino@Facundos-MacBook-Pro.local>
2020-02-05 10:49:42 -0300

    finish

--- plot4.R
+++ plot4.R
@@ -12,14 +12,25 @@ epc <-subset(epc, Date == "2007-02-01" | Date == "2007-02-02")
 # Create DateTime Column
 epc$DateTime <- strptime(paste(epc$Date, epc$Time), format="%Y-%m-%d %H:%M:%S")
 
+par(mfrow = c(2, 2))
+
+# Create plot1
+with(epc, plot(DateTime,Global_active_power, type = "l", ylab = "Global Active Power", xlab = ""))
+
+# Create plot2
+with(epc, plot(DateTime,Voltage, type = "l", ylab = "Voltage", xlab = "datetime"))
+
 # Create plot3
 with(epc, {
         plot(DateTime,Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab = "")
         lines(DateTime,Sub_metering_2, type = "l", col = "red")
         lines(DateTime,Sub_metering_3, type = "l", col = "blue")
-        legend("topright", lwd = 1, col = c("black" ,"red", "blue"), legend = paste0("Sub_metering_", 1:3))
-         })
+        legend("topright", lwd = 1, bty = "n", col = c("black" ,"red", "blue"), legend = paste0("Sub_metering_", 1:3))
+})
+
+# Create plot4
+with(epc, plot(DateTime,Global_reactive_power, type = "l", ylab = "Global_reactive_power", xlab = "datetime"))
 
 # Export to PNG
-dev.copy(png, file = "plot3.png")
+dev.copy(png, file = "plot4.png")
 dev.off()
